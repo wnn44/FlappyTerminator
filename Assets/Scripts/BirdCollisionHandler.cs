8f353b71ff459e917fa8d20445ed76d91020de2e
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2025-04-08 22:28:32 +0300

    GameOver

--- Assets/Scripts/BirdCollisionHandler.cs
+++ Assets/Scripts/BirdCollisionHandler.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 [RequireComponent(typeof(Bird))]
 public class BirdCollisionHandler : MonoBehaviour
 {
+    public bool IsAlive { get; private set; } = true;
     public event Action CollisionDetected;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Ground ground))
         {
-            Debug.Log("Земля");            
             CollisionDetected?.Invoke();
+        }
+
+        if (other.TryGetComponent(out Bullet bullet))
+        {
+            IsAlive = false;
+            Debug.Log("Отлетался");
         }
     }
 }
--- Assets/Scripts/BirdMover.cs
+++ Assets/Scripts/BirdMover.cs
@@ -8,12 +8,13 @@ public class BirdMover : MonoBehaviour
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _maxRotationZ;
     [SerializeField] private float _minRotationZ;
+    [SerializeField] private BirdCollisionHandler _birdCollisionHandler;
 
     private Vector3 _startPosition;
     private Quaternion _startRotation;
     private Rigidbody2D _rigidbody2D;
-    private Quaternion _maxRotation;
-    private Quaternion _minRotation;
+    //private Quaternion _maxRotation;
+    //private Quaternion _minRotation;
 
     private void Start()
     {
@@ -21,8 +22,8 @@ public class BirdMover : MonoBehaviour
         _startRotation = transform.rotation;
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
-        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
-        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+        //_maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        //_minRotation = Quaternion.Euler(0, 0, _minRotationZ);
 
         Reset();
     }
@@ -31,7 +32,7 @@ public class BirdMover : MonoBehaviour
     {
         float targetAngle;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _birdCollisionHandler.IsAlive)
         {
             _rigidbody2D.velocity = new Vector2(_speed, _tapForce);
         }
